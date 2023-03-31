b2555e82817aa03dde7ec550dba024b4769e6553
Ricardo Contreras <ricardo.contreras@hermosillo.com>
2023-03-31 00:12:36 -0700

    Test of page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'boquitas-sanas'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
+  basePath,
+  assetPrefix,
   output: 'export',
+  distDir: 'out',
 }
 
 module.exports = nextConfig
