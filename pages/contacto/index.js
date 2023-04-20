641bc17a348bfb467d4c665c99f87eab57cb568b
Ricardo Contreras <ricardo.contreras@hermosillo.com>
2023-04-20 00:21:48 -0700

    Size of index and map

--- pages/contacto/index.js
+++ pages/contacto/index.js
@@ -42,10 +42,10 @@ export default function Contacto() {
           <div className='column' style={{
             alignItems: 'center',
           }}>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3358.6995636533297!2d-115.46140708461317!3d32.66743819687124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d7701a91ca6a15%3A0xa2d37b00f9e5814e!2sDra.%20Hebe%20Ng%20odontopediatra!5e0!3m2!1ses!2smx!4v1678941101197!5m2!1ses!2smx" width="600" height="450" style={{
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3358.6995636533297!2d-115.46140708461317!3d32.66743819687124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d7701a91ca6a15%3A0xa2d37b00f9e5814e!2sDra.%20Hebe%20Ng%20odontopediatra!5e0!3m2!1ses!2smx!4v1678941101197!5m2!1ses!2smx" style={{
               border: 0,
               borderRadius: '10px',
-              width: '40vw',
+              width: '60vw',
               height: '40vh',
           }}allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
